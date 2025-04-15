fb1c91af7f4a8c4aeb45b3e40b5966880f930060
Emil Soman <emil.soman@gmail.com>
2025-04-15 20:06:59 +0530

    Add rustler precompilation configs

--- lib/baml_elixir/native.ex
+++ lib/baml_elixir/native.ex
@@ -1,10 +1,11 @@
-defmodule BamlClient.Resume do
-  defstruct [:company, :job_title, :name]
-end
-
 defmodule BamlElixir.Native do
-  use Rustler,
-    otp_app: :baml_elixir
+  version = Mix.Project.config()[:version]
+
+  use RustlerPrecompiled,
+    otp_app: :baml_elixir,
+    base_url: "https://github.com/emilsoman/baml-elixir/releases/download/v#{version}/",
+    force_build: System.get_env("BAML_ELIXIR_BUILD") in ["1", "true"],
+    version: version
 
   def call(_client, _function_name, _args), do: :erlang.nif_error(:nif_not_loaded)
 end
