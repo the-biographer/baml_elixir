4727b16fff9df85abccc9cdbfb2b8f715ed29542
Emil Soman <emil.soman@gmail.com>
2025-06-23 21:21:36 +0530

    Update checksum

--- checksum-Elixir.BamlElixir.Native.exs
+++ checksum-Elixir.BamlElixir.Native.exs
@@ -1,4 +1,4 @@
 %{
-  "libbaml_elixir-v1.0.0-pre.13-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:57ed5814e5c3a60326582afeff000d272f6ac416bdc8bed81b5471613524f9be",
-  "libbaml_elixir-v1.0.0-pre.13-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5a022d90f6c6a6decebd46975e1002accefa48b8dcffe294d615e0a385bc452f",
+  "libbaml_elixir-v1.0.0-pre.14-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:429e56769dc44c01824803927c002d43e236e70cd0b6a648c142a83ea5da7d40",
+  "libbaml_elixir-v1.0.0-pre.14-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:98a7b295011e7b506e46e178e54e453ed0cc9499bbbeffcb7ba7290e7b02285b",
 }
