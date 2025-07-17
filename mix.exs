f481e438aa3505056adef06c68ee9ad654f20557
avi-turetsky <averyanturetsky@gmail.com>
2025-07-16 23:52:45 -0700

    Revert Elixir version requirement back to 1.17

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule BamlElixir.MixProject do
       app: :baml_elixir,
       description: "Call BAML functions from Elixir.",
       version: @version,
-      elixir: "~> 1.18.4",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package()
