96abd310743ad90ffd2d6de380215644161ad147
avi-turetsky <averyanturetsky@gmail.com>
2025-07-17 00:03:52 -0700

    Update checksums for v1.0.0-pre.16 release

--- checksum-Elixir.BamlElixir.Native.exs
+++ checksum-Elixir.BamlElixir.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libbaml_elixir-v1.0.0-pre.15-nif-2.15-aarch64-apple-darwin.so.tar.gz" =>
-    "sha256:680b6f0c12bf3e4aadb49b074b6ef1299d10273a36fb17fff5a0eb95389a0446",
-  "libbaml_elixir-v1.0.0-pre.15-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:0ca6444f35f1d2c64d68d08ae7b05edbfe297a8b9508ce2d7bde4b5c8aa7fefb"
+  "libbaml_elixir-v1.0.0-pre.16-nif-2.15-aarch64-apple-darwin.so.tar.gz" =>
+    "sha256:04ce1c9e9095d17264a0fcb61d13304500631892cf3a13e84367a442b91fb91a",
+  "libbaml_elixir-v1.0.0-pre.16-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:399eb42574fccf739b647754bce9908dd778fa16dfc5e00fb22ab82639ce9c76"
 }
